be3694ae5ba191a702c41bd20129cc0970b1f286
faaazy <160858681+faaazy@users.noreply.github.com>
2024-03-04 21:00:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ function blueColor() {
   ).innerHTML = `rgb(${red}, ${green}, ${blue})`;
   document.getElementById("bg-color").style.backgroundColor = fullColor;
 }
+
+function randomize() {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  redParam = getRandomInt(0, 256);
+  greenParam = getRandomInt(0, 256);
+  blueParam = getRandomInt(0, 256);
+
+  red = redParam;
+  green = greenParam;
+  blue = blueParam;
+  document.getElementById("blue-label").innerHTML = blue;
+  document.getElementById("red-label").innerHTML = red;
+  document.getElementById("green-label").innerHTML = green;
+
+  fullColor = document.getElementById(
+    "color-text"
+  ).innerHTML = `rgb(${redParam}, ${greenParam}, ${blueParam})`;
+  document.getElementById("bg-color").style.backgroundColor = fullColor;
+}
